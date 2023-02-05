50583f4940084c7f9c0630cf993fb81f406f2af1
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-06 02:44:16 +0400

    Add files via upload

---
+++ bitvector.py
@@ -0,0 +1,27 @@
+class BitVector:
+    def __init__(self, size):
+        self.size = size
+        self.vector = [0] * (size // 64 + 1)
+        
+    def set(self, index):
+        self.vector[index // 64] |= 1 << (index % 64)
+    
+    def reset(self, index):
+        self.vector[index // 64] &= ~ (1 << (index % 64))
+
+    
+    def show(self, index):
+        return (self.vector[index // 64] >> (index % 64)) & 1
+        return (self.vector[index // 64] >> (index % 64)) & 1
+
+
+
+b = BitVector(989)
+b.set(7)
+print(b.show(7))
+b.set(357)
+b.reset(7)
+print(b.show(7))
+print(b.show(357))
+
+
