720b124afb77aaefed40d024cfdb97f5185fe2ac
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-06 02:55:16 +0400

    New Commit

--- bitvector.py
+++ bitvector.py
@@ -4,15 +4,17 @@ class BitVector:
         self.vector = [0] * (size // 64 + 1)
         
     def set(self, index):
-        self.vector[index // 64] |= 1 << (index % 64)
-    
+        idx = index // 64
+        self.vector[idx] |= 1 << (index % 64)
+
     def reset(self, index):
-        self.vector[index // 64] &= ~ (1 << (index % 64))
+        idx = index // 64
+        self.vector[idx // 64] &= 0 << (index % 64)
 
     
     def show(self, index):
-        return (self.vector[index // 64] >> (index % 64)) & 1
-
+        idx = index // 64
+        return (self.vector[idx] >> (index % 64))
 
 b = BitVector(989)
 b.set(7)
@@ -21,5 +23,4 @@ b.set(357)
 b.reset(7)
 print(b.show(7))
 print(b.show(357))
-
 
